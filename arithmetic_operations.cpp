e107b0108841f8a720c8280cf4d6aca9f30b2ace
ZERO SUS <suszero9@gmail.com>
2026-07-25 00:48:22 +0530

    Create arithmetic_operations.cpp

---
+++ arithmetic_operations.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main(){
+	int s =5;
+	int r =9;
+	
+	std::cout << "A = " <<s <<'\n'<<"B = " <<r<<'\n';
+	std::cout << "Addition of A and B is  : "<< s+r <<'\n';
+	std::cout << "Substraction of A and B is  : "<< s-r <<'\n';
+	std::cout << "Multiplication of A and B is  : "<< s*r <<'\n';
+	std::cout << "Division of A and B is  : "<< double(s)/r <<'\n';
+	std::cout << "reminder of A and B is  : "<< s%r <<'\n';
+	
+	
+	
+	return 0;
+}
