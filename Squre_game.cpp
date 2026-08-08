79165b5f7d4422551d764e7df9281b84dc99daff
ZERO SUS <suszero9@gmail.com>
2026-08-08 17:54:06 +0530

    Add files via upload

---
+++ Squre_game.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+	int row;
+	int col;
+	char ch;
+	std::cout<<"Enter The Character to do rectangle shape: ";
+	std::cin>>ch;
+	std::cout<<"Enter The Number of rows : ";
+	std::cin>>row;
+	std::cout<<"Enter The Number os Columns : ";
+	std::cin>>col;
+	for(int j=1;j<=col;j++){
+		for(int i=1;i<=row;i++){
+			std::cout<<ch;}
+	std::cout<<'\n';
+	}
+	
+	
+	return 0;
+}
