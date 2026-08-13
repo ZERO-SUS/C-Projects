e5aea86676250be4a5e0d3590b9d5ae25b1c308f
ZERO SUS <suszero9@gmail.com>
2026-08-14 00:06:21 +0530

    Add files via upload

---
+++ Arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main(){
+	std:: string color[] = {"red", "blue","green","yellow"};
+	std::cout<< color[0]<<'\n';
+	std::cout<< color[1]<<'\n';
+	std::cout<< color[2]<<'\n';
+	std::cout<< color[3]<<'\n';
+	
+	
+	
+	return 0;
+}
